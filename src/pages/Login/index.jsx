053190d510f110b39b9a0d9d9f5bb9bbfbd702ee
hndf48 <isrhael.souza@motorolasolutions.com>
2019-01-21 14:44:01 -0300

    row and col

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
+import { Row, Col } from "antd";
 import { WrappedLoginForm as LoginForm } from "../../components/LoginForm";
 import "./styles.css";
 
 export default class Login extends Component {
   render() {
     return (
-      <div className="login-container">
-        <div className="img-container">
+      <Row>
+        <Col span={12}>
           <img src="#" alt="" className="img-login" />
-        </div>
-        <div className="form-container">
+        </Col>
+        <Col span={12}>
           <h1 className="login-title">Login</h1>
           <p className="login-subtitle">Entre no sistema</p>
           <LoginForm />
-        </div>
-      </div>
+        </Col>
+      </Row>
     );
   }
 }
