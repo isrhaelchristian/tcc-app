c8aef42926576e96d928c61e2eb7d29bfe2bdb7b
hndf48 <isrhael.souza@motorolasolutions.com>
2019-03-28 15:27:58 -0300

    search explore

--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -6,6 +6,10 @@ import SearchOptions from '../../components/SearchOptions';
 
 import styles from "./styles";
 
+const sheltersImg = "https://images.unsplash.com/photo-1545305948-59e8b64b8264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1398&q=80";
+const dogsIMg = "https://images.unsplash.com/photo-1444212477490-ca407925329e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80";
+const catsIMg = "https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1348&q=80";
+
 export default class SearchResults extends Component {
   render() {
     return (
@@ -15,6 +19,37 @@ export default class SearchResults extends Component {
         </div>
         <div className={css(styles.content)}>
           <SearchOptions />
+          <div className={css(styles.exploreContainer)}>
+            <div className={css(styles.exploreTitleContainer)}>
+              <p className={css(styles.exploreTitle)}>Explorar Localidade</p>
+            </div>
+            <div className={css(styles.exploreItemsContainer)}>
+              <div className={css(styles.exploreItemContainer)}>
+                <div className={css(styles.exploreImgContainer)}>
+                  <img alt="shelters" src={sheltersImg} className={css(styles.exploreItemImg)} />
+                </div>
+                <div className={css(styles.exploreItemTitleContainer)}>
+                  <p className={css(styles.exploreItemTitle)}>Abrigos</p>
+                </div>
+              </div>
+              <div className={css(styles.exploreItemContainer)}>
+                <div className={css(styles.exploreImgContainer)}>
+                  <img alt="shelters" src={dogsIMg} className={css(styles.exploreItemImg)} />
+                </div>
+                <div className={css(styles.exploreItemTitleContainer)}>
+                  <p className={css(styles.exploreItemTitle)}>Cães</p>
+                </div>
+              </div>
+              <div className={css(styles.exploreItemContainer)}>
+                <div className={css(styles.exploreImgContainer)}>
+                  <img alt="shelters" src={catsIMg} className={css(styles.exploreItemImg)} />
+                </div>
+                <div className={css(styles.exploreItemTitleContainer)}>
+                  <p className={css(styles.exploreItemTitle)}>Gatos</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/pages/SearchResult/styles.js
+++ src/pages/SearchResult/styles.js
@@ -28,9 +28,75 @@ const styles = StyleSheet.create({
     width: '100vw',
     height: '92vh',
     display: 'flex',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'flex-start',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
+  },
+  exploreContainer: {
+    width: "100%",
+    height: "20h",
+    marginTop: "5vh"
+  },
+  exploreTitleContainer: {
+    width: "100%",
+    height: "5vh",
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingLeft: "5vw",
+  },
+  exploreTitle: {
+    fontFamily: fonts.family,
+    fontWeight: "bold",
+    fontSize: "18pt",
+    margin: 0
+  },
+  exploreItemsContainer: {
+    width: "100%",
+    height: "15vh",
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingLeft: "5vw",
+  },
+  exploreItemContainer: {
+    width: "15vw",
+    height: "10vh",
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center",
+    border: "1px solid",
+    borderColor: colors.dark,
+    marginRight: "40px",
+    borderRadius: "5px",
+    boxShadow: "rgba(0, 0, 0, 0.06) 0px 2px 4px 0px"
+  },
+  exploreImgContainer: {
+    width: "7vw",
+    height: "10vh",
+    overflow: "hidden"
+  },
+  exploreItemImg: {
+    width: "7vw",
+    height: "10vh",
+  },
+  exploreItemTitleContainer: {
+    width: "8vw",
+    height: "10vh",
+    display: "flex",
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  exploreItemTitle: {
+    color: colors.darker,
+    fontWeight: "bold",
+    fontSize: "12pt",
+    fontFamily: fonts.family,
+    margin: 0
   }
 });
 
