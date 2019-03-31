849e3c6aa558b6cd20f47d8ea97831ebe7d17944
isrhaelc <isrhael.christian@gmail.com>
2019-03-31 16:55:58 -0300

    changes

--- src/components/ImagesGrid/styles.js
+++ src/components/ImagesGrid/styles.js
@@ -1,39 +1,46 @@
-import {
-  StyleSheet
-} from 'aphrodite';
+import { StyleSheet } from "aphrodite";
 
 const styles = StyleSheet.create({
   container: {
-    width: '30vw',
+    width: "30vw",
     height: "50vh",
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    marginTop: 20
   },
   mainImgContainer: {
-    width: '100%',
-    height: "70%",
-    overflow: "hidden"
+    width: "100%",
+    height: "80%",
+    overflow: "hidden",
+    borderRadius: 5,
+    boxShadow: "rgba(0, 0, 0, 0.06) 0px 2px 4px 0px"
   },
   mainImg: {
-    width: '100%',
-    height: 'auto',
+    width: "100%",
+    height: "auto"
   },
   carouselContainer: {
     width: "100%",
-    height: "30%",
-    display: 'flex',
-    flexDirection: 'row',
+    height: "20%",
+    display: "flex",
+    flexDirection: "row"
   },
   thumbnailImgs: {
-    width: "20%",
+    width: "18%",
     height: "100%",
-    overflow: 'hidden',
+    overflow: "hidden",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5,
+    borderRadius: 5
   },
   thumbail: {
-    width: 'auto',
+    width: "auto",
     height: "100%",
+    boxShadow: "rgba(0, 0, 0, 0.06) 0px 2px 4px 0px"
   }
 });
 
--- src/pages/PetDetail/styles.js
+++ src/pages/PetDetail/styles.js
@@ -1,35 +1,33 @@
-import {
-  StyleSheet
-} from 'aphrodite';
-import { colors } from '../../settings';
+import { StyleSheet } from "aphrodite";
+import { colors } from "../../settings";
 
 const styles = StyleSheet.create({
   container: {
-    width: '100vw',
-    display: 'flex',
-    alignContent: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
+    width: "100vw",
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "column"
   },
   header: {
-    width: '100vw',
-    height: '8vh',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingLeft: '1vw',
-    paddingRight: '1vw',
-    borderBottom: '1px solid',
-    borderBottomColor: colors.dark,
+    width: "100vw",
+    height: "8vh",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: "1vw",
+    paddingRight: "1vw",
+    borderBottom: "1px solid",
+    borderBottomColor: colors.dark
   },
   content: {
-    width: '100vw',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
+    width: "100vw",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default styles;
